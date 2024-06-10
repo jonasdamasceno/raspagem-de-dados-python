f1011a5de4f4f5d465c18f1481fe5a117f165782
jonas michael oliveira damasceno <jonas@meuvidraceiro.com.br>
2024-06-10 09:17:52 -0300

    teste para ver se o git está funcionando

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,32 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = []
+
+    for news in search_news({"title": {"$regex": title, "$options": "i"}}):
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = []
+
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+
+        for news in search_news({"timestamp": {"$regex": formatted_date}}):
+            result.append((news["title"], news["url"]))
+
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
